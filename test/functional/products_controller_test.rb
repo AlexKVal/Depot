e872c0b4f8dbc5ca90c4dca23041ccceb469d663
Shimme <sashainkiev@gmail.com>
2011-06-17 19:48:30 +0300

    Added store index and tests

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,11 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_description', :minimum => 3
+    assert_select '#main .list_image', 3
+    assert_select 'dt', 'Programming Ruby 1.9'
+    assert_select '.list_line_odd', :minimum => 1
+    assert_select '.list_line_even', :minimum => 1
   end
 
   test "should get new" do
