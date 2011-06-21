32e11d09e9db1415e29e113a1268ea694bb4b6d9
Shimme <sashainkiev@gmail.com>
2011-06-21 14:29:51 +0300

    Added admin-panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	before_filter :authorize
+    protect_from_forgery
 
   private
 
@@ -10,4 +11,12 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       cart
     end
+    
+  protected
+	
+	def authorize
+		  unless User.count.zero? || User.find_by_id(session[:user_id])
+		    redirect_to login_url, :notice=>"Please log in"
+		  end	
+	end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,16 @@
 class StoreController < ApplicationController
+  skip_before_filter :authorize
+  
   def index
-  	@products = Product.all
-  	@cart = current_cart
-  	
-  	if session[:access_counter].nil?
-  	    session[:access_counter] = 1
-  	else
-  	  	session[:access_counter]+=1
-  	end
-  	@access_counter = session[:access_counter]
+    @products = Product.all
+    @cart = current_cart
+    
+    if session[:access_counter].nil?
+        session[:access_counter] = 1
+    else
+        session[:access_counter]+=1
+    end
+    @access_counter = session[:access_counter]
   end
 
 end
