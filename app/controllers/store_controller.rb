527b0e16e806b52a794aaf2df2941ff0a99844b8
Shimme <sashainkiev@gmail.com>
2011-06-21 17:24:54 +0300

    Added I18n

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,15 +2,18 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
   
   def index
-    @products = Product.all
-    @cart = current_cart
-    
-    if session[:access_counter].nil?
-        session[:access_counter] = 1
-    else
-        session[:access_counter]+=1
-    end
-    @access_counter = session[:access_counter]
+  	if params[:set_locale]
+  		redirect_to store_path(:locale=>params[:set_locale])
+  	else
+  		@products = Product.all
+    	@cart = current_cart
+    	
+    	if session[:access_counter].nil?
+        	session[:access_counter] = 1
+    	else
+        	session[:access_counter]+=1
+    	end
+    	@access_counter = session[:access_counter]
+  	end
   end
-
 end
