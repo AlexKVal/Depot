d2f03bfb5e53a335e467474bc1afd0d2b31c7319
Sasha <sashainkiev@gmail.com>
2011-10-11 21:55:31 +0300

    add debug output to app_template

---
+++ app/controllers/missmeth_controller.rb
@@ -0,0 +1,12 @@
+class MissmethController < ApplicationController
+  skip_before_filter :authorize
+  
+  if ::Rails.env == "development"
+    def method_missing(name, *args)
+      render(:inline => %{
+               <h2>Unknown action: #{name}</h2>
+               Here are the request parameters:<br/>
+               <%= debug(params) %> })
+    end
+  end
+end
