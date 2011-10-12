b9fce3cab8a099ad170b0eaa97c8a12316685ebe
Sasha <sashainkiev@gmail.com>
2011-10-12 18:16:07 +0300

    clean up files

--- vendor/bundle/ruby/1.8/gems/looksee-1.0.2/ext/extconf.rb
+++
@@ -1,9 +0,0 @@
-ruby_engine = Object.const_defined?(:RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
-extension = ruby_engine == 'ruby' ? 'mri' : ruby_engine
-
-require 'mkmf'
-$CPPFLAGS << " -DRUBY_VERSION=#{RUBY_VERSION.tr('.', '')}"
-if extension == 'mri'
-  $CPPFLAGS << " -Imri/1.9.2" if RUBY_VERSION >= '1.9.2'
-end
-create_makefile "looksee/#{extension}", extension
